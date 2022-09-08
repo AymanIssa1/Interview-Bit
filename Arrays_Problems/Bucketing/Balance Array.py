d24675ca9f69e896ee87ea901173a77122900b90
Ayman <aymanadel003@gmail.com>
2022-09-08 15:39:37 +0200

    "Balance Array" Solved - Solution 2 ( without time limit exceeded )

--- Arrays_Problems/Bucketing/Balance Array.py
+++ Arrays_Problems/Bucketing/Balance Array.py
@@ -24,6 +24,37 @@ class Solution:
         return specialElementsCount
 
 
+class Solution2:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        specialElementsCount = 0
+        oddCount = 0
+        evenCount = 0
+
+        for index in range(0, len(A)):
+            if index % 2 == 0:
+                evenCount += A[index]
+            elif index % 2 == 1:
+                oddCount += A[index]
+
+        oddLeftCount = 0
+        evenLeftCount = 0
+        for index in range(0, len(A)):
+            if index % 2 == 0:
+                if evenLeftCount + oddCount == evenCount - A[index] + oddLeftCount:
+                    specialElementsCount += 1
+                evenLeftCount += A[index]
+                evenCount -= A[index]
+            elif index % 2 == 1:
+                if oddLeftCount + evenCount == oddCount - A[index] + evenLeftCount:
+                    specialElementsCount += 1
+                oddLeftCount += A[index]
+                oddCount -= A[index]
+
+        return specialElementsCount
+
+
 print(Solution.solve(None, [2, 1, 6, 4]))
 print(Solution.solve(None, [5, 5, 2, 5, 8]))
 print(Solution.solve(None, [2, 8, 2, 2, 6, 3]))
