51fc600bd0b9eb5403a0179a14f0263c4f66b84d
Ayman <aymanadel003@gmail.com>
2022-09-09 21:00:28 +0200

    "Is Rectangle?" Solved - Solution

---
+++ Math/Adhoc/Is Rectangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @param C : integer
+    # @param D : integer
+    # @return an integer
+    def solve(self, A, B, C, D):
+        if A == B and C == D:
+            return 1
+        return 0
